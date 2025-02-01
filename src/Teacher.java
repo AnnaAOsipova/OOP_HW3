d67babe39df87913899f7f58ee091ad02f7a466b
Anna <anna.a.osipova@outlook.com>
2025-02-01 20:30:11 +0200

    Teacher class created

---
+++ src/Teacher.java
@@ -0,0 +1,29 @@
+
+import java.util.List;
+
+public class Teacher extends User {
+    private Long teacherId;
+    private List<Teacher> teacherList;
+
+    public Teacher(String name, String lastName, String surName) {
+        super(name, lastName, surName);
+    }
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
+    public List<Teacher> getTeacherList() {
+        return teacherList;
+    }
+
+    public void setTeacherList(List<Teacher> teacherList) {
+        this.teacherList = teacherList;
+    }
+
+    public void createTeacher(String firstName, String surName, String lastName) {
+        teacherList.add(new Teacher(firstName, lastName, surName));
+    }
+}
