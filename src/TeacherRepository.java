17521f393cabb07957b7af81c2b8d2334355267f
Anna <anna.a.osipova@outlook.com>
2025-02-10 00:24:45 +0200

    ЕуфсрукКузщышещкнTeacherRepository created

---
+++ src/TeacherRepository.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public interface TeacherRepository {
+    void addTeacher(Teacher teacher);
+    void editTeacherName(String name, String newName);
+    void editTeacherSurName(String surName, String newSurName);
+    void editTeacherLastName(String lastName, String newLastName);
+    List<Teacher> getAllTeachers();
+
+    void createTeacher(String name, String surName, String lastName);
+}
+
+/*
+OCP - интерфейс позволяет добавлять новые реализации, не изменяя при этом основной код классов
+ */
